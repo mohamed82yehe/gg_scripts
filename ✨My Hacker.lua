927e72c0672424ea5872053f8facdcbbc0687fcf
mohamed82yehe <85711495+mohamed82yehe@users.noreply.github.com>
2025-05-28 01:50:03 +0300

    Update ✨My Hacker.lua

--- ✨My Hacker.lua
+++ ✨My Hacker.lua
@@ -535,7 +535,7 @@ end
 
 
 function RESET_SKY_ANIMAL()
-    gg.alert( لازم تكون فاتح حيوان السحاب ‌:⁠-⁠[⁩)
+    gg.alert(" لازم تكون فاتح حيوان السحاب ‌:⁠-⁠["⁩)
     gg.clearResults()
     gg.searchNumber("120;70~150", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1, 0)
     local results = gg.getResults(100000)
