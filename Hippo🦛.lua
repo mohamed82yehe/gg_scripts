7fab4b091e9ab5a9ad9ca3f75dd9903ce1ab11bd
mohamed82yehe <85711495+mohamed82yehe@users.noreply.github.com>
2025-08-04 07:39:45 +0300

    Update Hippo🦛.lua

--- Hippo🦛.lua
+++ Hippo🦛.lua
@@ -1,9 +1,9 @@
+gg.alert("ابعت طعم السمك اولا 🐠 بعد اكتمال البحث")
 gg.setVisible(false)
 gg.clearResults()
 
 -- البحث عن القيم الثلاثة 200;20;5 من نوع DOUBLE
-gg.searchNumber("200;20;5", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
-gg.sleep(1000)
+gg.searchNumber("1;20;1500", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
 
 -- حفظ كل النتائج في متغير
 local allResults = gg.getResults(1000)
@@ -16,11 +16,10 @@ for i = 1, #r1 do
   r1[i].value = 999999
 end
 gg.setValues(r1)
-gg.sleep(500)
 
 -- فلترة القيمة 5 وتعديلها إلى 0
 gg.loadResults(allResults) -- استعادة نفس النتائج مرة أخرى
-gg.refineNumber("5", gg.TYPE_DOUBLE)
+gg.refineNumber("1", gg.TYPE_DOUBLE)
 local r2 = gg.getResults(100)
 for i = 1, #r2 do
   r2[i].value = 0
